219afd133f673a2b1b2e4b4bf60c80966a47baad
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-05-13 18:37:53 -0300

    Iniciado a criação da tela de Serviços

---
+++ React-Native-Expo-Gatito-Static/src/screens/ServicesScreen.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StatusBar, View, Text } from 'react-native'
+
+const servicos = [
+	{
+		id: 1,
+		name: 'Banho',
+		price: 79.9,
+		description: 'Não dê banho no seu gato! Mas se precisar nós damos!'
+	},
+	{
+		id: 2,
+		name: 'Vacina V4',
+		price: 89.9,
+		description: 'Uma dose da vacina V4. Seu gato precisa de duas.'
+	},
+	{
+		id: 3,
+		name: 'Vacina Antirrábica',
+		price: 99.9,
+		description: 'Uma dose da vacina antirrábica. Seu gato precisa de uma por ano.'
+	}
+]
+
+const ServicesScreen = () => {
+	return (
+		<View>
+			<StatusBar />
+			<Text>Tela de Serviços!</Text>
+		</View>
+	)
+}
+
+export default ServicesScreen
