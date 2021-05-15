b17f2ffaeca003fa0f2fa97cef66b46e3a387fee
Alex Xavier <ajxv20@gmail.com>
2021-05-15 07:06:29 -0300

    Renderizado estilo e demais estruturas dos itens

--- React-Native-Expo-Gatito-Static/src/components/Item.js
+++ React-Native-Expo-Gatito-Static/src/components/Item.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
+
+import { itemStyle } from '../styles'
 
 const Item = ({ name, price, description }) => {
-	return <>
-		<Text>{ name }</Text>
-		<Text>{ price }</Text>
-		<Text>{ description }</Text>
-	</>
+	return (
+		<View>
+			<View style={itemStyle.information}>
+				<Text style={itemStyle.name}>{ name }</Text>
+				<Text style={itemStyle.description}>{ description }</Text>
+				<Text style={itemStyle.price}>{ price }</Text>
+			</View>
+
+			<View style={itemStyle.shoppingCart}>
+				<View>
+					<View style={itemStyle.value}>
+						<Text style={itemStyle.description}>Quantidade:</Text>
+						<TextInput value='0'/>
+					</View>
+
+					<View style={itemStyle.value}>
+						<Text style={itemStyle.description}>Preço:</Text>
+						<Text style={itemStyle.price}>0</Text>
+					</View>
+				</View>
+
+				<Button title='Adicionar' />
+			</View>
+
+			<View style={itemStyle.divisor} />
+		</View>
+	)
 }
 
 export default Item
