cb6ab0350a4b3b1802976db0b2957bcb655f6876
Alex Xavier <ajxv20@gmail.com>
2021-05-18 06:34:38 -0300

    Adicionado remoção de zeros a esquerda do valor

--- React-Native-Expo-Gatito-Static/src/components/CampoInteiro.js
+++ React-Native-Expo-Gatito-Static/src/components/CampoInteiro.js
@@ -5,6 +5,10 @@ const CampoInteiro = ({ value, action }) => {
   const update = ( newValue, callback ) => {
     const verifyInteger = newValue.match(/^[0-9]*$/)
     if (!verifyInteger) return
+
+    const removeLeftZero = newValue.replace(/^(0)(.+)/, '$2')
+
+    callback(removeLeftZero)
   }
 
   const numberToString = String(value)
