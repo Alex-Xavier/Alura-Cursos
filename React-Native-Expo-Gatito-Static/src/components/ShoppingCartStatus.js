2c59aae74767aa37b25e623a9e7df8d1eb1e45e0
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-05-26 09:44:30 -0300

    Corrigido componente + Adicionado concluir pedido

--- React-Native-Expo-Gatito-Static/src/components/ShoppingCartStatus.js
+++ React-Native-Expo-Gatito-Static/src/components/ShoppingCartStatus.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import Button from './Button'
+
 import { statusShoppingCartStyle as styles } from '../styles'
 
 const ShoppingCartStatus = ({ total }) => {
-  <View style={styles.content}>
-    <View style={styles.total}>
-      <Text style={styles.description}>Total do carrinho:</Text>
+  return (
+    <View style={styles.content}>
+      <View style={styles.total}>
+        <Text style={styles.description}>Total do carrinho:</Text>
 
-      <Text style={styles.value}>
-        {
-          Intl.NumberFormat('pt-BR', {
-            style: 'currency', currency: 'BRL'
-          }).format(total)
-        }
-      </Text>
+        <Text style={styles.value}>
+          {
+            Intl.NumberFormat('pt-BR', {
+              style: 'currency', currency: 'BRL'
+            }).format(total)
+          }
+        </Text>
+      </View>
+
+      <View style={styles.button}>
+        <Button value='Concluir pedido' inverse />
+      </View>
     </View>
-  </View>
+  )
 }
 
 export default ShoppingCartStatus
