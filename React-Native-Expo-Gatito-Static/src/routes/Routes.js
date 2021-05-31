aa1c2d76b583570f6686c0b035f5f55966ef96bf
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-05-31 18:16:14 -0300

    Estilizado o Tab Navigator

--- React-Native-Expo-Gatito-Static/src/routes/Routes.js
+++ React-Native-Expo-Gatito-Static/src/routes/Routes.js
@@ -5,12 +5,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { ServicesScreen, ShoppingCartScreen } from '../screens'
 
+import { colors } from '../styles'
+
 const Tab = createBottomTabNavigator()
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: colors.purple,
+          inactiveTintColor: colors.white,
+          activeBackgroundColor: colors.purple,
+          inactiveBackgroundColor: colors.orange,
+          style: {
+            height: 70
+          },
+          labelStyle: {
+            width: '100%',
+            flex: 1,
+            fontWeight: 'bold',
+            fontSize: 16,
+            lineHeight: 21,
+            marginTop: 3,
+            paddingTop: 21,
+            backgroundColor: colors.orange
+          }
+        }}>
         <Tab.Screen name='Serviços' component={ServicesScreen} />
         <Tab.Screen name='Carrinho' component={ShoppingCartScreen} />
       </Tab.Navigator>
