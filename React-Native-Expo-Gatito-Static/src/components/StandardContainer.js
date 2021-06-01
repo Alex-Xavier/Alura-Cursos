db68728e236a7d1b3c30238b37531b5f8cc82b4a
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-05-31 21:05:31 -0300

    Estilizado barras do iOS e Android

--- React-Native-Expo-Gatito-Static/src/components/StandardContainer.js
+++ React-Native-Expo-Gatito-Static/src/components/StandardContainer.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { SafeAreaView, StatusBar, KeyboardAvoidingView, Platform } from 'react-native'
 
-import { globalStyle } from '../styles'
+import { globalStyle, colors } from '../styles'
 
 const StandardContainer = ({ children }) => {
   return (
-    <SafeAreaView style={globalStyle.fill}>
-      <StatusBar />
+    <>
+      <SafeAreaView style={globalStyle.adjustScreenUp}>
+        <StatusBar backgroundColor={colors.purple} />
 
-			<KeyboardAvoidingView
-				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-				style={globalStyle.fill}>
-          {children}
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+		  	<KeyboardAvoidingView
+		  		behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+		  		style={globalStyle.fill}>
+            {children}
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+
+      <SafeAreaView style={globalStyle.adjustScreenLow} />
+    </>
   )
 }
 
