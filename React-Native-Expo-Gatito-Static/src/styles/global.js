42077235b558ac45e6d24fffe899e9ca6e11e1a1
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-05-31 21:04:32 -0300

    Adicionado objetos de estilo para as barras do iOS

--- React-Native-Expo-Gatito-Static/src/styles/global.js
+++ React-Native-Expo-Gatito-Static/src/styles/global.js
@@ -1,15 +1,23 @@
 import { StyleSheet } from 'react-native'
 
-export default StyleSheet.create({
-  fill: {
-    flex: 1
-  }
-})
-
 export const colors = {
   purple: '#A050BE',
   orange: '#FABE50',
-  blackish: '#555555',
+  blackish: '#555',
   white: '#fff',
   gray: '#C7C7C7'
 }
+
+export default StyleSheet.create({
+  fill: {
+    flex: 1
+  },
+  adjustScreenUp: {
+    flex: 1,
+    backgroundColor: colors.purple
+  },
+  adjustScreenLow: {
+    flex: 0,
+    backgroundColor: colors.orange
+  }
+})
