d35311bb1114bd5ab19e38d3dac8b8dc2998c0b6
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-06-04 21:50:53 -0300

    Adicionado/iniciado estilização sacola de compras

--- React-Native-Design-System-Lighteria/App.js
+++ React-Native-Design-System-Lighteria/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 
 import StandardContainer from './src/components/StandardContainer'
 
 const App = () => {
   return (
     <StandardContainer>
-      <Text>Aplicativo Lighteria!</Text>
+      <View>
+        <Text>Lighteria!</Text>
+
+        <Image
+          source={require('./assets/images/icone-sacola.png')}
+          style={styles.shoppingBag} />
+      </View>
     </StandardContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  shoppingBag: {
+    height: 30,
+    width: 30
+  }
+})
 
 export default App
