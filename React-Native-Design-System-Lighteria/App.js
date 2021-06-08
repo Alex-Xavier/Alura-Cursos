c33b2338b07979f0a18c60b85d14d50e63e8cba6
Alex Xavier <ajxv20@gmail.com>
2021-06-08 07:51:53 -0300

    Componentizado o cabeçalho

--- React-Native-Design-System-Lighteria/App.js
+++ React-Native-Design-System-Lighteria/App.js
@@ -1,71 +1,14 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
 
 import StandardContainer from './src/components/StandardContainer'
+import Header from './src/components/Header'
 
 const App = () => {
   return (
     <StandardContainer>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>LIGHTERIA</Text>
-
-        <View style={styles.shoppingBagContainer}>
-          <Image
-            source={require('./assets/images/icone-sacola.png')}
-            style={styles.shoppingBag} />
-        </View>
-      </View>
-
-      <View style={styles.descriptionContainer}>
-        <View style={styles.spacer} />
-        
-        <View style={styles.descriptionTextContainer}>
-          <Text style={styles.descriptionText}>Categorias</Text>
-        </View>   
-      </View>
+      <Header />
     </StandardContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    height: 120,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 24
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  shoppingBagContainer: {
-    backgroundColor: '#FFF',
-    padding: 18,
-    borderRadius: 30
-  },
-  shoppingBag: {
-    height: 30,
-    width: 30
-  },
-  descriptionContainer: {
-    paddingHorizontal: 24
-  },
-  spacer: {
-    borderWidth: 0.5,
-    borderColor: '#A1A5AA'
-  },
-  descriptionTextContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: -46
-  },
-  descriptionText: {
-    padding: 34,
-    backgroundColor: '#F4F0F4',
-    fontSize: 16,
-    color: '#A1A5AA'
-  }
-})
 
 export default App
