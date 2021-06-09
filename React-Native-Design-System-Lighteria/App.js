82e7932fc1d81649f22742e5cc0c65af20785b14
Alex Xavier <ajxv20@gmail.com>
2021-06-09 06:44:36 -0300

    Criado a tela de Lista Produtos

--- React-Native-Design-System-Lighteria/App.js
+++ React-Native-Design-System-Lighteria/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import StandardContainer from './src/components/StandardContainer'
-import Header from './src/components/Header'
+import ProductList from './src/screens/ProductList'
 
 const App = () => {
   return (
     <StandardContainer>
-      <Header />
+      <ProductList />
     </StandardContainer>
   );
 }
---
+++ React-Native-Design-System-Lighteria/src/screens/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from 'react-native'
+
+import Header from '../components/Header'
+
+const ProductList = () => {
+  return (
+    <Header />
+  )
+}
+
+export default ProductList
