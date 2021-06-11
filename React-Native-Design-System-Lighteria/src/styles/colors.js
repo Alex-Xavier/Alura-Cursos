52d974035ffeca6fded29feb50297eef6b521700
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-06-10 22:10:30 -0300

    Criado estilos globais

---
+++ React-Native-Design-System-Lighteria/src/styles/colors.js
@@ -0,0 +1,14 @@
+export const BACKGROUND_COLOR = '#F4F0F4';
+export const BLACK = '#000';
+export const WHITE = '#FFF';
+export const COLOR_ACTION_BUTTON = '#00adef';
+
+// ACTIONS
+export const SUCCESS = '#3adb76';
+export const WARNING = '#ffae00';
+export const ALERT = '#cc4b37';
+
+// GRAYSCALE
+export const GRAY_LIGHT = '#e6e6e6';
+export const GRAY_MEDIUM = '#cacaca';
+export const GRAY_DARK = '#8a8a8a';
