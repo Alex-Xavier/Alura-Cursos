33b7b2840ac73a657c1cbef7688f42a39355f7d4
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-06-12 17:34:01 -0300

    Renderizado Background e Descrição do Item

--- React-Native-Design-System-Lighteria/src/screens/ProductDetails.js
+++ React-Native-Design-System-Lighteria/src/screens/ProductDetails.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+
+import Background from '../components/Background'
+import ItemDescription from '../components/ItemDescription'
 
 const ProductDetails = ({ route }) => {
   const { title, image, studio, description, name, price, id } = route.params
 
   return (
     <View style={styles.container}>
-      <Text>{ title }</Text>
+      <Background />
+
+      <ItemDescription
+        title={title}
+        image={image}
+        studio={studio}
+        description={description}
+        name={name}
+        price={price}
+        id={id}
+      />
     </View>
   )
 }
