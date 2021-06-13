9b1c15237602af9ec424eed7f4852e900fbc15a6
Alex Xavier <ajxv20@gmail.com>
2021-06-13 08:00:28 -0300

    Instanciada tela de Checkout

--- React-Native-Design-System-Lighteria/src/routes/Routes.js
+++ React-Native-Design-System-Lighteria/src/routes/Routes.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import ProductList from '../screens/ProductList'
 import ProductDetails from '../screens/ProductDetails'
+import Checkout from '../screens/Checkout'
 
 const Stack = createStackNavigator()
 
@@ -20,6 +21,12 @@ const Routes = () => {
         <Stack.Screen
           name='Detalhes do Produto'
           component={ProductDetails}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name='Checkout'
+          component={Checkout}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
