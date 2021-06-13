87ef14e3bdfe5636860f116a78339e25f1b8bdf3
Alex Xavier <ajxv20@gmail.com>
2021-06-13 07:54:03 -0300

    Implementado o ContextAPI para a tela de Checkout

--- React-Native-Design-System-Lighteria/App.js
+++ React-Native-Design-System-Lighteria/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import StandardContainer from './src/components/StandardContainer'
 
+import Provider from './src/provider/provider'
+
 import Routes from './src/routes/Routes'
 
 const App = () => {
   return (
     <StandardContainer>
-      <Routes />
+      <Provider>
+        <Routes />
+      </Provider>
     </StandardContainer>
   );
 }
